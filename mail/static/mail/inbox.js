f9531285ebc7e03a70779b5be7622bdccf2ade13
TastePate <pavelarz14@gmail.com>
2026-09-20 16:37:22 +0400

    bug fixed

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -75,10 +75,6 @@ function render_email(email, mailbox) {
     const email_div = document.createElement('div');
     email_div.className = 'mailbox-element';
 
-    if (email.read) {
-        email_div.classList.add('read');
-    }
-
     email_div.addEventListener('click', event => {
        event.preventDefault();
        render_email_page(email, mailbox==='sent');
